2040ff039bb3aeb8771f191dab8d21197f9e273c
jevvk <9702805+jevvk@users.noreply.github.com>
2022-08-04 18:06:54 +0200

    update logger

--- pkg/cloud/apiproxy/server.go
+++ pkg/cloud/apiproxy/server.go
@@ -3,27 +3,28 @@ package apiproxy
 import (
 	"context"
 	"fmt"
-	"log"
 	"net/http"
 
 	"edge.knative.dev/pkg/cloud/apiproxy/authentication"
 	"edge.knative.dev/pkg/cloud/apiproxy/websockets"
 	"golang.org/x/net/http2"
 	"golang.org/x/net/http2/h2c"
+	"sigs.k8s.io/controller-runtime/pkg/log"
 )
 
 func New(ctx context.Context, addr string, clientManager *websockets.ClientManager) *http.Server {
 	authenticator, err := authentication.NewFromLocalFiles()
+	log := log.FromContext(ctx)
 
 	if err != nil {
 		panic(fmt.Errorf("could not create authenticator: %s", err))
 	}
 
 	websocketHandler := websockets.NewHandler(clientManager)
-	log.Printf("Created websocket handler.")
+	log.Info("Created websocket handler.")
 
 	authenticatorHandler := authenticator.NewHandler(websocketHandler)
-	log.Printf("Created authenticator.")
+	log.Info("Created authenticator.")
 
 	server := &http.Server{
 		Addr:    addr,

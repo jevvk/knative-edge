19367cbe6d0d2665b1e18fe3d6b7d39069c15177
jevvk <9702805+jevvk@users.noreply.github.com>
2022-08-05 10:35:34 +0200

    add edge resource crd and move edge cluster to root group

---
+++ pkg/apis/edge/edgeresource_types.go
@@ -0,0 +1,60 @@
+/*
+Copyright 2022 jevv k.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EdgeResourceSpec defines the desired state of EdgeResource
+type EdgeResourceSpec struct {
+	// The resourceVersion of the remote resource
+	// +kubebuilder:printcolumn
+	RemoteResourceVersion string `json:"resource.remote.version"`
+	// The definition of the remote resource
+	Data string `json:"data"`
+}
+
+// EdgeResourceStatus defines the observed state of EdgeResource
+type EdgeResourceStatus struct {
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+//+kubebuilder:resource:scope=Namespace
+
+// EdgeResource is the Schema for the edgeclusters API
+type EdgeResource struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   EdgeResourceSpec   `json:"spec,omitempty"`
+	Status EdgeResourceStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// EdgeResourceList contains a list of EdgeResource
+type EdgeResourceList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []EdgeResource `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&EdgeResource{}, &EdgeResourceList{})
+}
--- pkg/apis/edge/groupversion_info.go
+++ pkg/apis/edge/groupversion_info.go
@@ -14,9 +14,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// Package v1 contains API Schema definitions for the edge v1 API group
+// Package v1 contains API Schema definitions for the cloud v1 API group
 //+kubebuilder:object:generate=true
-//+groupName=edge.knative.dev
+//+groupName=internal.edge.knative.dev
 package v1
 
 import (
@@ -26,7 +26,7 @@ import (
 
 var (
 	// GroupVersion is group version used to register these objects
-	GroupVersion = schema.GroupVersion{Group: "edge.knative.dev", Version: "v1"}
+	GroupVersion = schema.GroupVersion{Group: "internal.edge.knative.dev", Version: "v1"}
 
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
 	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}

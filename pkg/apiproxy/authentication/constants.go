a2f17c1b182f300f0f289f3f190a7bcf5d3f0e92
jevvk <9702805+jevvk@users.noreply.github.com>
2022-08-07 22:50:07 +0200

    update secrets path

--- pkg/apiproxy/authentication/constants.go
+++ pkg/apiproxy/authentication/constants.go
@@ -6,7 +6,7 @@ const (
 	AuthHeader               = "X-K-EDGE-Authorization"
 	Namespace                = "knative-edge-system"
 	SecretName               = "knative-edge-certificates"
-	AuthenticationPath       = "/var/run/secrets/knative.dev/edge/authentication"
+	AuthenticationPath       = "/var/run/secrets/jevv.dev/edge/authentication"
 	CertificateAuthorityFile = "ca.pem"
 	PrivateKeyFile           = "priv.pem"
 )

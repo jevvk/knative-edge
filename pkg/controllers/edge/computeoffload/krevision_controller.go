3822c5e54f77da5cabce57dea9a519f9a4f95c80
jevvk <9702805+jevvk@users.noreply.github.com>
2022-08-13 18:28:04 +0200

    move revision creation into its own function

--- pkg/controllers/edge/computeoffload/krevision_controller.go
+++ pkg/controllers/edge/computeoffload/krevision_controller.go
@@ -5,11 +5,16 @@ import (
 	"strings"
 
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/tools/record"
 
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/builder"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
+	"sigs.k8s.io/controller-runtime/pkg/source"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"knative.dev/pkg/kmeta"
@@ -65,11 +70,11 @@ func (r *KRevisionReconciler) Reconcile(ctx context.Context, request ctrl.Reques
 	var shouldCreate = false
 	var shouldUpdate = false
 	var shouldDelete = false
-	var revision servingv1.Revision
+	var revision *servingv1.Revision
 
 	revisionNamespacedName := getRevisionNamespacedName(request.NamespacedName)
 
-	if err := r.Get(ctx, revisionNamespacedName, &revision); err != nil {
+	if err := r.Get(ctx, revisionNamespacedName, revision); err != nil {
 		if !apierrors.IsNotFound(err) {
 			return ctrl.Result{}, err
 		}
@@ -81,32 +86,12 @@ func (r *KRevisionReconciler) Reconcile(ctx context.Context, request ctrl.Reques
 		shouldDelete = true
 	}
 
-	if shouldCreate || shouldUpdate {
-		revision = servingv1.Revision{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:            revisionNamespacedName.Name,
-				Namespace:       revisionNamespacedName.Namespace,
-				OwnerReferences: []metav1.OwnerReference{*kmeta.NewControllerRef(&service)},
-				Labels: map[string]string{
-					controllers.ManagedLabel:   "true",
-					controllers.EdgeLocalLabel: "true",
-					controllers.CreatedByLabel: "knative-edge-computeoffload-controller",
-					controllers.ManagedByLabel: "knative-edge-computeoffload-controller",
-				},
-				Annotations: map[string]string{
-					controllers.KnativeNoGCAnnotation: "true",
-				},
-			},
-			// TODO: create remote proxy
-			// TODO: pass domain mapping from remote
-			// TODO: set concurrency and timeout
-			// TODO: pass http proxy envs
-			Spec: servingv1.RevisionSpec{},
-		}
-	}
+	// TODO: check if we should update
 
 	if shouldCreate {
-		if err := r.Create(ctx, &revision); err != nil {
+		revision = r.buildRevision(revisionNamespacedName, &service)
+
+		if err := r.Create(ctx, revision); err != nil {
 			if apierrors.IsConflict(err) {
 				return ctrl.Result{Requeue: true}, nil
 			}
@@ -114,7 +99,9 @@ func (r *KRevisionReconciler) Reconcile(ctx context.Context, request ctrl.Reques
 			return ctrl.Result{}, err
 		}
 	} else if shouldUpdate {
-		if err := r.Update(ctx, &revision); err != nil {
+		newRevision := r.buildRevision(revisionNamespacedName, &service)
+
+		if err := r.Update(ctx, newRevision); err != nil {
 			if apierrors.IsConflict(err) {
 				return ctrl.Result{Requeue: true}, nil
 			}
@@ -122,7 +109,7 @@ func (r *KRevisionReconciler) Reconcile(ctx context.Context, request ctrl.Reques
 			return ctrl.Result{}, err
 		}
 	} else if shouldDelete {
-		if err := r.Delete(ctx, &revision); err != nil {
+		if err := r.Delete(ctx, revision); err != nil {
 			if apierrors.IsNotFound(err) {
 				return ctrl.Result{}, nil
 			}
@@ -132,6 +119,30 @@ func (r *KRevisionReconciler) Reconcile(ctx context.Context, request ctrl.Reques
 	}
 
 	return ctrl.Result{}, nil
+}
+
+func (r *KRevisionReconciler) buildRevision(namespacedName types.NamespacedName, owner *servingv1.Service) *servingv1.Revision {
+	return &servingv1.Revision{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:            namespacedName.Name,
+			Namespace:       namespacedName.Namespace,
+			OwnerReferences: []metav1.OwnerReference{*kmeta.NewControllerRef(owner)},
+			Labels: map[string]string{
+				controllers.ManagedLabel:   "true",
+				controllers.EdgeLocalLabel: "true",
+				controllers.CreatedByLabel: "knative-edge-computeoffload-controller",
+				controllers.ManagedByLabel: "knative-edge-computeoffload-controller",
+			},
+			Annotations: map[string]string{
+				controllers.KnativeNoGCAnnotation: "true",
+			},
+		},
+		// TODO: create remote proxy
+		// TODO: pass domain mapping from remote
+		// TODO: set concurrency and timeout
+		// TODO: pass http proxy envs
+		Spec: servingv1.RevisionSpec{},
+	}
 }
 
 func (r *KRevisionReconciler) Setup(mgr ctrl.Manager) error {

22ac3470c09ac88755aef6bc7a153f1f79f5496d
jevvk <9702805+jevvk@users.noreply.github.com>
2022-08-14 09:42:36 +0200

    only mirror remote if generation has changed

--- pkg/controllers/utils.go
+++ pkg/controllers/utils.go
@@ -7,5 +7,25 @@ import (
 )
 
 func UpdateLastGenerationAnnotation(src, dst client.Object) {
-	dst.GetAnnotations()[LastGenerationAnnotation] = fmt.Sprintf("%d", src.GetGeneration())
+	annotations := dst.GetAnnotations()
+
+	if annotations == nil {
+		annotations = make(map[string]string)
+		dst.SetAnnotations(annotations)
+	}
+
+	annotations[LastGenerationAnnotation] = fmt.Sprintf("%d", src.GetGeneration())
+}
+
+func UpdateLabels(object client.Object) {
+	labels := object.GetLabels()
+
+	if labels == nil {
+		labels = make(map[string]string)
+		object.SetAnnotations(labels)
+	}
+
+	labels[ManagedLabel] = "true"
+	labels[ManagedByLabel] = "knative-edge"
+	labels[CreatedByLabel] = "knative-edge-controller"
 }

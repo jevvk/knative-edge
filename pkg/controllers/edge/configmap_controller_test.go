c21fcc862f705d771d3cb000ec5ab2c3262a40f1
jevvk <9702805+jevvk@users.noreply.github.com>
2022-10-13 07:26:33 +0200

    start work on controllers tests

---
+++ pkg/controllers/edge/configmap_controller_test.go
@@ -0,0 +1,54 @@
+package edge
+
+import (
+	"context"
+	"time"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+
+	edgecontrollers "edge.jevv.dev/pkg/controllers"
+)
+
+var _ = Describe("configmap controller", func() {
+	const (
+		timeout  = time.Second * 1
+		duration = time.Second * 10
+		interval = time.Millisecond * 250
+	)
+
+	Context("when creating new resource", func() {
+		It("should replicate resources", func() {
+			By("creating a configmap")
+			ctx := context.Background()
+
+			configMap := &corev1.ConfigMap{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "configmap-test",
+					Namespace: "default",
+					Labels: map[string]string{
+						edgecontrollers.EnvironmentLabel: "testA",
+					},
+				},
+				Data: map[string]string{
+					"foo":  "bar",
+					"fooo": "baar",
+				},
+			}
+
+			Expect(remoteClusterClient.Create(ctx, configMap)).Should(Succeed())
+
+			namespacedName := types.NamespacedName{Name: "foo", Namespace: "default"}
+			mirroredConfigMap := &corev1.ConfigMap{}
+
+			Eventually(func() bool {
+				err := edgeClusterClient.Get(ctx, namespacedName, mirroredConfigMap)
+				return err == nil
+			}, timeout, interval).Should(BeTrue())
+		})
+	})
+})
---
+++ pkg/controllers/edge/suite_test.go
@@ -0,0 +1,123 @@
+package edge
+
+import (
+	"context"
+	"path/filepath"
+	"testing"
+
+	ctrl "sigs.k8s.io/controller-runtime"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/rest"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/cluster"
+	"sigs.k8s.io/controller-runtime/pkg/envtest"
+	"sigs.k8s.io/controller-runtime/pkg/envtest/printer"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	edgev1alpha1 "edge.jevv.dev/pkg/apis/edge/v1alpha1"
+	servingv1 "edge.jevv.dev/pkg/apis/serving/v1"
+)
+
+func TestRun(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecsWithCustomReporters(t, "Edge controller Integration Suite", []Reporter{printer.NewlineReporter{}})
+}
+
+var stop context.CancelFunc
+
+var edgeClusterCfg *rest.Config
+var remoteClusterCfg *rest.Config
+
+var edgeClusterTestEnv *envtest.Environment
+var remoteClusterTestEnv *envtest.Environment
+
+var edgeClusterClient client.Client
+var remoteClusterClient client.Client
+
+var _ = BeforeSuite(func() {
+	logf.SetLogger(zap.New(zap.WriteTo(GinkgoWriter), zap.UseDevMode(true)))
+
+	edgeClusterTestEnv = &envtest.Environment{
+		CRDDirectoryPaths:     []string{filepath.Join("..", "..", "..", "config", "crd", "overlays", "edge")},
+		ErrorIfCRDPathMissing: true,
+	}
+
+	remoteClusterTestEnv = &envtest.Environment{
+		CRDDirectoryPaths:     []string{filepath.Join("..", "..", "..", "config", "crd", "overlays", "cloud")},
+		ErrorIfCRDPathMissing: true,
+	}
+
+	var err error
+
+	edgeClusterCfg, err = edgeClusterTestEnv.Start()
+	Expect(err).NotTo(HaveOccurred())
+	Expect(edgeClusterCfg).NotTo(BeNil())
+
+	remoteClusterCfg, err = remoteClusterTestEnv.Start()
+	Expect(err).NotTo(HaveOccurred())
+	Expect(remoteClusterCfg).NotTo(BeNil())
+
+	err = edgev1alpha1.AddToScheme(scheme.Scheme)
+	Expect(err).NotTo(HaveOccurred())
+
+	err = servingv1.AddToScheme(scheme.Scheme)
+	Expect(err).NotTo(HaveOccurred())
+
+	edgeClusterClient, err = client.New(edgeClusterCfg, client.Options{
+		Scheme: scheme.Scheme,
+	})
+	Expect(err).NotTo(HaveOccurred())
+
+	remoteClusterClient, err = client.New(remoteClusterCfg, client.Options{
+		Scheme: scheme.Scheme,
+	})
+	Expect(err).NotTo(HaveOccurred())
+
+	var ctx context.Context
+	ctx, stop = context.WithCancel(context.TODO())
+
+	go func() {
+		envs := []string{"testA", "testB"}
+
+		defer GinkgoRecover()
+
+		mgr, err := ctrl.NewManager(edgeClusterCfg, ctrl.Options{
+			Scheme: scheme.Scheme,
+		})
+		Expect(err).ToNot(HaveOccurred())
+
+		remoteCluster, err := cluster.New(remoteClusterCfg, func(o *cluster.Options) {
+			o.Scheme = scheme.Scheme
+			o.NewCache = EnvScopedCache(envs)
+		})
+		Expect(err).ToNot(HaveOccurred())
+
+		err = mgr.Add(remoteCluster)
+		Expect(err).ToNot(HaveOccurred())
+
+		hasEdgeLabelPredicate := HasEdgeSyncLabelPredicate(envs)
+
+		err = (&ConfigMapReconciler{
+			Client:        mgr.GetClient(),
+			Scheme:        mgr.GetScheme(),
+			Log:           mgr.GetLogger().WithName("configmap-controller"),
+			Recorder:      mgr.GetEventRecorderFor("configmap-controller"),
+			RemoteCluster: remoteCluster,
+			Envs:          envs,
+		}).SetupWithManager(mgr, hasEdgeLabelPredicate)
+		Expect(err).ToNot(HaveOccurred())
+
+		err = mgr.Start(ctx)
+		Expect(err).ToNot(HaveOccurred())
+	}()
+}, 60)
+
+var _ = AfterSuite(func() {
+	stop()
+	Expect(edgeClusterTestEnv.Stop()).To(Succeed())
+	Expect(remoteClusterTestEnv.Stop()).To(Succeed())
+})

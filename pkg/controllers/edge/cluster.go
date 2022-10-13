7071bb387f684e4cb90afac59d15a120bd7e6817
jevvk <9702805+jevvk@users.noreply.github.com>
2022-10-13 07:21:19 +0200

    fix remote cluster schema issue

--- pkg/controllers/edge/cluster.go
+++ pkg/controllers/edge/cluster.go
@@ -2,32 +2,25 @@ package edge
 
 import (
 	"fmt"
-	"os"
 
 	"k8s.io/client-go/tools/clientcmd"
 	"sigs.k8s.io/controller-runtime/pkg/cluster"
 )
 
 func NewRemoteClusterOrDie(opts ...cluster.Option) cluster.Cluster {
-	bytes, err := os.ReadFile(fmt.Sprintf("%s/%s", ConfigPath, KubeconfigFile))
+	kubeconfigPath := fmt.Sprintf("%s/%s", ConfigPath, KubeconfigFile)
 
-	if err != nil {
-		panic(fmt.Errorf("couldn't read remote cluster kubeconfig: %w", err))
-	}
+	loader := clientcmd.NewDefaultClientConfigLoadingRules()
+	loader.Precedence = append(loader.Precedence, kubeconfigPath)
 
-	config, err := clientcmd.NewClientConfigFromBytes(bytes)
-
-	if err != nil {
-		panic(fmt.Errorf("couldn't parse kubeconfig: %w", err))
-	}
-
-	kubeconfig, err := config.ClientConfig()
+	kubeconfig, err :=
+		clientcmd.NewNonInteractiveDeferredLoadingClientConfig(loader, nil).ClientConfig()
 
 	if err != nil {
 		panic(fmt.Errorf("couldn't retrieve kubeconfig: %w", err))
 	}
 
-	cluster, err := cluster.New(kubeconfig)
+	cluster, err := cluster.New(kubeconfig, opts...)
 
 	if err != nil {
 		panic(fmt.Errorf("couldn't create remote cluster: %w", err))
